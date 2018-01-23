093cb50b7e2b2e0ad7d2705deb50591b08c889d2
Megh Parikh <meghprkh@gmail.com>
2018-01-23 22:25:56 +0530

    Add / in link path if not there

--- src/Link.js
+++ src/Link.js
@@ -1,7 +1,9 @@
 import { Link } from "inferno-router";
 
 export default props => (
-  <Link {...props} to={process.env.PUBLIC_URL + props.to}>
+  <Link {...props} to={
+    process.env.PUBLIC_URL + (props.to.startsWith("/") ? props.to : "/" + props.to)
+  }>
     {props.children}
   </Link>
 );
